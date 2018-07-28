6ab7b7dc0608dd050196e062b9e1756a8f7134f7
Paul van Eyk <eyesnz@gmail.com>
2018-07-29 11:26:45 +1200

    Add config for which pin is used to connect to the dht22 on the RPI GPIO

--- weewx_service/extra_sensors_service.py
+++ weewx_service/extra_sensors_service.py
@@ -9,6 +9,9 @@ class ExtraSensorsService(StdService):
     def __init__(self, engine, config_dict):
         super(ExtraSensorsService, self).__init__(engine, config_dict)      
         d = config_dict.get('ExtraSensorsService', {})
+        # Read from config which pin to use on the RPI GPIO
+        # Defaults to 22
+        self.dht22_pin = d.get('dht22_pin', 22)
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.load_data)
     
     def load_data(self, event):
@@ -29,7 +32,7 @@ class ExtraSensorsService(StdService):
 
     # Get DHT22 data
     def get_dht22(self, event):
-        humidity, temperature = dht.read_retry(dht.DHT22, 22) #Use pin 22
+        humidity, temperature = dht.read_retry(dht.DHT22, self.dht22_pin)
         if humidity is not None:
             syslog.syslog(syslog.LOG_DEBUG, "extrasensors: found humidity value of %s" % humidity)
             event.record['inHumidity'] = float(humidity)
